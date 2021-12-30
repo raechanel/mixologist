23beaa7de36c8391f0e97eefadecb38d582757e0
regan hayes <hayesregan@yahoo.com>
2021-12-30 03:30:02 -0500

    added foundation

---
+++ js/index.js
@@ -0,0 +1,15 @@
+'use strict';
+
+'use strict';
+
+// Button
+
+// let button = document.getElementById('start');
+
+// function handleSubmit(event) {
+//   event.preventDefault();
+//   window.location.href='drinks.html';
+// }
+
+
+// button.addEventListener('submit', handleSubmit);
